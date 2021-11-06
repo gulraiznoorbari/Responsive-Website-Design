0285c7972d85289482902c7ac4cea7ddaffcc254
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2021-11-06 16:39:36 +0500

    Adding Dark Mode

--- Projects/Starbucks_Landing_Page/script.js
+++ Projects/Starbucks_Landing_Page/script.js
@@ -5,9 +5,15 @@ function changeCircleColor(color) {
     const circle = document.querySelector(".circle");
     circle.style.background = color;
 }
-function toggleMenu(color) {
+function toggleMenu() {
     const menuToggle = document.querySelector(".toggle");
     const navigation = document.querySelector(".navigation");
     menuToggle.classList.toggle("active");
     navigation.classList.toggle("active");
+}
+function themeSwitch() {
+    const themeSwitch = document.querySelector(".themeSwitch");
+    const body = document.querySelector("body");
+    themeSwitch.classList.toggle("active");
+    body.classList.toggle("dark");
 }
